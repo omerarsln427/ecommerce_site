27ef0a3e566a31bb5573d152733979632bab5026
Omer Arslan <omerarsln21@gmail.com>
2026-06-11 16:03:07 +0300

    Fix profile routes and test normal user access

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\ProfileController;
 use App\Models\Product;
 
 Route::get('/', function () {
@@ -29,6 +30,12 @@ Route::get('/checkout/success/{order}', [CheckoutController::class, 'success'])-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::prefix('admin')->name('admin.')->middleware('admin')->group(function () {
     Route::resource('products', ProductController::class);
