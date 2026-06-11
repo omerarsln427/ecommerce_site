e501a9d272de6a4e59cc335802b9590e6b994a0b
Omer Arslan <omerarsln21@gmail.com>
2026-06-11 15:35:35 +0300

    Protect admin panel with admin middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+
+        if (!auth()->user()->hasRole('admin')) {
+            abort(403, 'You are not authorized to access this page.');
+        }
+
+        return $next($request);
+    }
+}
